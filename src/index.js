4f4b9bd35f6eccb8a94c193e78dcabd8012c1127
Yevhen <40315142+EugeneKolom@users.noreply.github.com>
2023-08-16 22:32:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsDict = Object.fromEntries(bracketsConfig.map(([a, b]) => [b, a]))
+  const leftBrackets = Object.values(bracketsDict)
+
+  const queue = []
+
+  for (const bracket of str) {
+    if (bracket in bracketsDict && (!leftBrackets.includes(bracket) || queue.at(-1) === bracket)) {
+      const lastBracket = queue.pop()
+      if (lastBracket != bracketsDict[bracket]) return false
+    } else {
+      queue.push(bracket)
+    }
+  }
+  return !queue.length
 }
